39f8ba0dd42edf77db47b1ddad88e21c58ce2ad6
刘刊 <kan@cpan.org>
2011-04-21 04:47:03 +0000

    Import of KAN/DynGig-Range-0.01 from CPAN.
    gitpan-cpan-distribution: DynGig-Range
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KAN/DynGig-Range-0.01.tar.gz
    gitpan-cpan-author:       KAN
    gitpan-cpan-maturity:     released

---
+++ lib/DynGig/Range.pm
@@ -0,0 +1,68 @@
+=head1 NAME
+
+DynGig::Range - A collection of Range modules
+
+=cut
+package DynGig::Range;
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+our $VERSION = '0.01';
+
+=head1 MODULES
+
+=head2 DynGig::Range::Integer
+
+Integer set arithmetics.
+
+=head2 DynGig::Range::String 
+
+Set arithemtics.
+
+=head2 DynGig::Range::CLI::String
+
+CLI for DynGig::Range::String - expand/digest/count
+
+=head2 DynGig::Range::Integer::Object 
+
+Implements DynGig::Range::Interface::Object.
+
+=head2 DynGig::Range::Integer::Parse 
+
+Implements DynGig::Range::Interface::Parse.
+
+=head2 DynGig::Range::Interface::Object 
+
+Base class for range objects
+
+=head2 DynGig::Range::Interface::Parse 
+
+Base class for Range parsers
+
+=head2 DynGig::Range::String::Object 
+
+Implements DynGig::Range::Interface::Object.
+
+=head2 DynGig::Range::String::Parse 
+
+Implements DynGig::Range::Interface::Parse.
+
+=head1 AUTHOR
+
+Kan Liu
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2010. Kan Liu
+
+This program is free software; you may redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
+__END__
---
+++ lib/DynGig/Range/CLI/String.pm
@@ -0,0 +1,105 @@
+=head1 NAME
+
+DynGig::Range::CLI::String - CLI for Range::String - expand/digest/count
+
+=cut
+package DynGig::Range::CLI::String;
+
+use warnings;
+use strict;
+use Carp;
+
+use IO::Select;
+use Pod::Usage;
+use Getopt::Long;
+
+use DynGig::Range::String;
+use DynGig::Util::Sysrw;
+use DynGig::Util::CLI;
+
+$| ++;
+
+=head1 EXAMPLE
+
+ use DynGig::Range::CLI::String;
+
+ DynGig::Range::CLI::String->main( delimiter => "\n" );
+
+=head1 SYNOPSIS
+
+$exe B<--help>
+
+[echo range .. |] $exe range ..
+
+[echo range .. |] $exe range .. B<--count>
+
+[echo range .. |] $exe range .. B<--expand> [B<--delimiter> token]
+
+=cut
+sub main
+{
+    my ( $class, %option ) = @_;
+
+    map { croak "$_ not defined" if ! defined $option{$_} } qw( delimiter );
+
+    my $delimiter = $option{delimiter};
+
+    $delimiter =~ s/\n/newline/g;
+    $delimiter =~ s/\t/tab/g;
+
+    my $menu = DynGig::Util::CLI->new
+    (
+        'h|help',"print help menu",
+        'c|count','count of elements',
+        'e|expand','expand into a list',
+        'delimiter=s',"[ $delimiter ]",
+    );
+
+    my %pod_param = ( -input => __FILE__, -output => \*STDERR );
+
+    Pod::Usage::pod2usage( %pod_param )
+        unless Getopt::Long::GetOptions( \%option, $menu->option() );
+
+    if ( $option{h} )
+    {
+        warn join "\n", "Default value in [ ]", $menu->string(), "\n";
+        return 0;
+    }
+
+    croak "poll: $!\n" unless my $select = IO::Select->new();
+
+    my ( $buffer, $length );
+
+    $select->add( *STDIN );
+
+    map { $length = DynGig::Util::Sysrw->read( $_, $buffer ) }
+        $select->can_read( 0.1 );
+
+    push @ARGV, split /\s+/, $buffer if $length;
+
+    Pod::Usage::pod2usage( %pod_param ) unless @ARGV;
+
+    my $range = DynGig::Range::String->new( \@ARGV );
+
+    if ( $option{e} )
+    {
+        local $, = $option{delimiter};
+        local $\ = "\n";
+
+        print $range->list();
+    }
+    else
+    {
+        printf "%s\n", $option{c} ? $range->size() : $range->string();
+    }
+}
+
+=head1 NOTE
+
+See DynGig::Range
+
+=cut
+
+1;
+
+__END__
